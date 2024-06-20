0e7cdff8c14d0ba4b872e44fd29d77e242cdf1ec
ftichy <59052141+ftichy@users.noreply.github.com>
2024-06-20 08:40:32 +0200

    ADD MIGRATION

---
+++ GameStore.Api/Data/DataExtensions.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Data
+{
+    public static class DataExtensions
+    {
+        public static void MigrateDb(this WebApplication app)
+        {
+
+            using var scope = app.Services.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+            dbContext.Database.Migrate();
+        }
+
+    }
+}
